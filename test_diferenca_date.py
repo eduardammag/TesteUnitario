d1ad94d18f5c778e5579eb3cdd586503a079e629
Eduarda Mesquita <eduardammag@gmail.com>
2023-09-20 08:44:36 -0300

    inicio do relatorio de erros

--- test_diferenca_date.py
+++ test_diferenca_date.py
@@ -3,16 +3,19 @@ import unittest
 import unittest
 from unittest.mock import patch
 
-from date_dif import diferenca_datgitte_console, diferenca_date_arquivo
+from date_dif import diferenca_date_console, diferenca_date_arquivo
 
 class TestDiferencaDates(unittest.TestCase):
 
+    #Teste falho para data como string
     def test_diferenca_date_console(self):
         # Redefine a entrada para simular a entrada do usuário
         with patch('builtins.input', side_effect=["10 de Setembro de 2023 - 20 de Setembro de 2023"]):
             resultado = diferenca_date_console()
             self.assertEqual(resultado, 10)
 
+
+    #Teste falho para data como arquivo 
     def test_diferenca_date_arquivo(self):
         # Cria um arquivo temporário com datas
         with open('datas_temporarias.txt', 'w', encoding='utf-8') as f:
@@ -23,3 +26,6 @@ class TestDiferencaDates(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+ 
+
+
